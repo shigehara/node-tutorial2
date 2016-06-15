f746abcd75667af7b0122e7a854550cedc31e08e
shigehara <n_shigehara@udstudio.co.jp>
2016-06-15 10:34:23 +0900

    [add] prefixを追加

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,17 @@
 var gulp = require("gulp");
 var sass = require("gulp-sass");
+var autoprefixer = require("gulp-autoprefixer");
 
 gulp.task("sass", function () {
   gulp.src("./sass/**/*.scss") //入力元
     .pipe(sass().on("error", sass.logError))
+    .pipe(autoprefixer({
+      browsers: ["last 2 versions", "ie >= 9", "Android >= 4","ios_saf >= 8"],
+      cascade: false
+    }))
     .pipe(gulp.dest("./css")); //出力先
 });
 
 gulp.task('watch', function () {
-    gulp.watch('./sass/**/*.scss', ['sass']);
+  gulp.watch('./sass/**/*.scss', ['sass']);
 });
